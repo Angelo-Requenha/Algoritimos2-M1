37da7770be643660d2c01927e443ecd7bc1e43e7
Angelo Requenha <angelorequenha@gmail.com>
2025-03-29 08:25:48 -0300

    1.0

---
+++ roguelike.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+
+using namespace std;
+
+#define TAM 40
+
+int main()
+{
+    srand(time(NULL));
+    ///ALERTA: NAO MODIFICAR O TRECHO DE CODIGO, A SEGUIR.
+        //INICIO: COMANDOS PARA QUE O CURSOR NAO FIQUE PISCANDO NA TELA
+        HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_CURSOR_INFO     cursorInfo;
+        GetConsoleCursorInfo(out, &cursorInfo);
+        cursorInfo.bVisible = false; // set the cursor visibility
+        SetConsoleCursorInfo(out, &cursorInfo);
+        //FIM: COMANDOS PARA QUE O CURSOR NAO FIQUE PISCANDO NA TELA
+        //INICIO: COMANDOS PARA REPOSICIONAR O CURSOR NO INICIO DA TELA
+        short int CX=0, CY=0;
+        COORD coord;
+        coord.X = CX;
+        coord.Y = CY;
+        //FIM: COMANDOS PARA REPOSICIONAR O CURSOR NO INICIO DA TELA
+    ///ALERTA: NAO MODIFICAR O TRECHO DE CODIGO, ACIMA.
+
+    bool revelaMapa[TAM][TAM] = {false};
+    int mapa[TAM][TAM];
+
+    for (int i=0;i<TAM;i++){
+        for (int j=0;j<TAM;j++){
+            if (i==TAM-TAM or j==TAM-TAM or j==TAM-1 or i ==TAM-1){
+                mapa[i][j] = 1;
+            } else{
+                mapa[i][j] = 0;
+            }
+        }   
+    }
+
+    //Posicao inicial do personagem no console
+    int x=1, y=1, ix = 10, iy = 10, vida = 3;
+    
+    while(vida> 0){
+        ///Posiciona a escrita no iicio do console
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+        for (int i=0;i<TAM;i++){
+            for (int j=0;j<TAM;j++){
+                if (i > x-5 && i < x+5 && j > y-5 && j < y+5){
+                    revelaMapa[i][j] = true;
+                }
+                if (i == x && j == y){
+                    cout<<char(36);
+                } else if (revelaMapa[i][j]==false){
+                    cout<<"-";
+                } else if(revelaMapa[i][j]==true){
+                    if (mapa[i][j] == 0){
+                        cout<<" ";
+                    } else if(mapa[i][j]==1){
+                        cout<<char(219);
+                    }
+                }
+            }
+            cout<<endl;
+        }
+
+        cout<<"Vida: "<<vida;
+
+        ///executa os movimentos
+         if (_kbhit()) {
+            char tecla = _getch();
+            int novo_x = x, novo_y = y;
+            
+            switch (tecla) {
+                case 72: case 'w': novo_x--; break;
+                case 80: case 's': novo_x++; break;
+                case 75: case 'a': novo_y--; break;
+                case 77: case 'd': novo_y++; break;
+            }
+            
+            if (x == ix and y == iy){
+                vida--;
+            }
+            
+            if (mapa[novo_x][novo_y] == 0) {
+                x = novo_x;
+                y = novo_y;
+            }
+        }
+    } //fim do laco do jogo
+
+    return 0;
+} //fim main
